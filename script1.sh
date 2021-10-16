b701f34984d9b3704f313128972b73a321cbef1c
Pol Riubrogent <pol.riubrogent@autonoma.cat>
2021-10-16 16:27:23 +0200

    Arxius .temp fins exercici 4

--- script1.sh
+++ script1.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
-cut -d',' -f 1,2,3,4,5,6,7,8,9,10,11,13,14,15 CAvideos.csv > CAvideos1.csv
-awk -F ',' 'NR==1 || $12=="True" || $12=="False"' CAvideos1.csv > CAvideos2.csv
-awk -F ',' 'NR==1 || $14=="False"|| $14!= "True" ' CAvideos2.csv > CAvideos3.csv
-awk -F"," '{OFS=FS}{ if ($8 <= 1000000) $15="Bueno"; else if ($8=="views") $15="Ranking_Views"; else if ($8 > 10000000) $15="Estrella"; else $15="Excelente";print}' CAvideos3.csv > CAvideos4.csv
+cut -d',' -f 1,2,3,4,5,6,7,8,9,10,11,13,14,15 CAvideos.csv > CAvideos1.temp
+awk -F ',' 'NR==1 || $12=="True" || $12=="False"' CAvideos1.temp > CAvideos2.temp
+awk -F ',' 'NR==1 || $14=="False"|| $14!= "True" ' CAvideos2.temp > CAvideos3.temp
+awk -F"," '{OFS=FS}{ if ($8 <= 1000000) $15="Bueno"; else if ($8=="views") $15="Ranking_Views"; else if ($8 > 10000000) $15="Estrella"; else $15="Excelente";print}' CAvideos3.temp > CAvideosFINAL.csv
+rm *.temp
